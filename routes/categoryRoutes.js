e922e03eb0cbdfc158563ee78da093d2b3f38923
e1z2e34A <164937279+e1z2e34A@users.noreply.github.com>
2026-06-29 03:02:40 +0100

    Update categoryRoutes.js

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Category = require("../models/Category");
+const Category = require("../models/category");
 
 // ==========================
 // Get all categories
