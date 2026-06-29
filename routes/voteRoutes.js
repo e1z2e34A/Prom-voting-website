acf6b69740d62b64f36e88dd6d8e78ddbd7e6c4d
e1z2e34A <164937279+e1z2e34A@users.noreply.github.com>
2026-06-29 03:02:53 +0100

    Update voteRoutes.js

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Vote = require("../models/Vote");
+const Vote = require("../models/vote");
 
 // Cast Vote
 router.post("/", async (req, res) => {
